a202869768a53211f60a4ee6ca242ac07b3c6025
Stefan van Kranen <stefan@vankranen.de>
2026-02-06 11:08:29 +0100

    Logging Issue

--- oldProject/Helper.php
+++ oldProject/Helper.php
@@ -40,7 +40,17 @@
         public static function log($message) {
             // $input = Factory::getApplication()->input;
             // $ip = $input->server->get('REMOTE_ADDR', '', '');
-            file_put_contents("debug.txt", date("d.m.Y H:i:s")."|$message\n",FILE_APPEND);
+            $baseDir = \dirname(__DIR__);
+            $logDir = $baseDir . '/var/log';
+            $logFile = $logDir . '/oldproject_debug.txt';
+            if (!is_dir($logDir)) {
+                @mkdir($logDir, 0777, true);
+            }
+            $line = date("d.m.Y H:i:s")."|$message\n";
+            if (@file_put_contents($logFile, $line, FILE_APPEND) === false) {
+                $fallback = rtrim(sys_get_temp_dir(), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . 'oldproject_debug.txt';
+                @file_put_contents($fallback, $line, FILE_APPEND);
+            }
         }
 
         public static function stampCheck($parameter_list) {
